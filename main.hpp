88ed147751ba802458a451fe2c6e67b97d0b30ee
Elijah Sayres <elijahsayres@gmail.com>
2024-02-20 14:24:49 -0800

    First Commit: malloc is causing Seg Fault when reading strings

--- main.hpp
+++ main.hpp
@@ -16,15 +16,33 @@ struct Student
 Student *makeStudent();
 void printStudent(Student *head);
 
-Student *makeStudent()
-{
-    /*******************************
-     * Code your program here
-     *******************************/
+Student *makeStudent(){
+    Student *head, *tmp, *prev;
+    ifstream file;
+    file.open("students.txt");
+    if(!file){
+        cerr << "FILE FAILED TO OPEN" << endl;
+        exit(0);
+    }
+
+    //prev = tmp;
+    for(int i = 0; i < N; i++){
+        tmp = (Student *)malloc(sizeof(Student));
+        file >> tmp->id >> tmp->name >> tmp->major;
+        tmp->next = NULL;
+        if(i == 0){
+            head = tmp;
+        } else{
+            prev->next = tmp;
+        }
+        prev = tmp;
+    }
+    return head;
 }
 void printStudent(Student *head)
 {
-    /*******************************
-     * Code your program here
-     *******************************/
+    for(int i = 0; i < 5; i++){
+        cout << head->id << " " << head->name << " " << head->major << endl;
+        head = head->next;
+    }
 }
